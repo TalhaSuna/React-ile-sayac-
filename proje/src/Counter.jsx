e0da675e6441c96abf136cf550f8415bf0e4c779
TalhaSuna <talhasuna2003@gmail.com>
2025-03-11 11:06:36 +0300

    first commit

---
+++ proje/src/Counter.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import './Counter.css';
+
+const Counter = () => {
+  const [count, setCount] = useState(() => {
+    const savedCount = localStorage.getItem("counterValue");
+    return savedCount ? parseInt(savedCount, 10) : 10;
+  });
+  
+  const [step, setStep] = useState(1); 
+
+  
+  useEffect(() => {
+    localStorage.setItem("counterValue", count);
+  }, [count]);
+
+  const handleInputChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    setStep(value);
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1 style={{ fontSize: "32px" }}>{count}</h1>
+      <div>
+        <button
+          className="buttonStyle"
+          onClick={() => setCount(count + step)} 
+        >
+          +
+        </button>
+        <button
+          className="buttonStyle"
+          onClick={() => setCount(count - step)}
+        >
+          -
+        </button>
+        <button
+          className="buttonStyle"
+          onClick={() => setCount(0)} 
+        >
+          0
+        </button>
+        <input
+          type="number"
+          className="input"
+          defaultValue={1}
+          onChange={handleInputChange} 
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
